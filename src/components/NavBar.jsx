93776ce26c3e786b3337a3a917ea188859396c5b
Madavis1 <madavis120@gmail.com>
2025-10-04 23:25:23 +0100

    Remove excess files Create basic navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import "../css/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="container">
+      <a className="nav-link" href="">About Me</a>
+      <a className="nav-link" href="">Timeline</a>
+      <a className="nav-link" href="">Projects</a>
+      <a className="nav-link" href="">Contacts</a>
+    </div>
+  );
+};
+
+export default NavBar;
