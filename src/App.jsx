f5a1de51b05b4474be746e181de095ad87f33256
Madavis1 <madavis120@gmail.com>
2025-10-06 22:41:35 +0100

    Restart, design change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import NavBar from "./components/NavBar";
-import AboutMe from "./components/AboutMe";
 
 const App = () => {
   return (
     <>
-      <NavBar />
-      <AboutMe />
+      
     </>
   );
 };
