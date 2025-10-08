2edd355b24147f8f4b55f40d93946f010e7855c6
Madavis1 <madavis120@gmail.com>
2025-10-08 01:03:10 +0100

    Add arrow animation Start filling in info

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,32 @@
 import "../css/AboutMe.css";
+import Profile from '../assets/stock-user.jpg'
+import LinkedIn from '../assets/linkedin.svg'
 
 const AboutMe = () => {
-  return ( 
-  <div className="box">
-    <div className="inner">
+  return (
+    <div className="page">
+      <div className="box">
+        <div className="inner">
+          <img src={Profile} alt="Picture of Matthew Davis" />
+          <h1>Matthew Davis</h1>
+          <h2>Junior Software Developer</h2>
+          <h3>📍 Warwickshire, England</h3>
+          <div className="socials">
+            <a href="https://www.linkedin.com/in/your-linkedin-profile/">
 
+            </a>
+            <button>Github</button>
+            <button>Email</button>
+          </div>
+          <button>Download CV</button>
+        </div>
+      </div>
+      <div className="arrow">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
-  </div>
   );
 };
 
